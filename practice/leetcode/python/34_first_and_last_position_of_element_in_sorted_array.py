579a7086a80dfa8878be21707276f858a2bfeb1d
Sameer Kelkar <sakelkar@cisco.com>
2024-08-27 12:16:04 +0530

    problem 34 python modification

--- practice/leetcode/python/34_first_and_last_position_of_element_in_sorted_array.py
+++ practice/leetcode/python/34_first_and_last_position_of_element_in_sorted_array.py
@@ -24,7 +24,4 @@ class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         l = bisect_left(nums, target)
         r = bisect_left(nums, target+1)
-        if l == r:
-            return [-1, -1]
-        else:
-            return [l, r-1]
+        return [-1, -1] if l == r else [l, r-1]
