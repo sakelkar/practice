118c91d6ba33da04f2f818ec012b276b0c8483dc
Sameer Kelkar <sakelkar@cisco.com>
2025-06-02 09:56:16 +0530

    python 387, LIS

--- practice/leetcode/blind75/python/300_longest_increasing_subsequence.py
+++ practice/leetcode/blind75/python/300_longest_increasing_subsequence.py
@@ -1,4 +1,5 @@
 from typing import List
+import bisect
 
 
 class Solution:
@@ -11,7 +12,7 @@ class Solution:
                     LIS[i] = max(LIS[i], 1 + LIS[j])
         return(max(LIS))
 
-    def lengthOfLIS(self, nums: List[int]) -> int:
+    def lengthOfLIS2(self, nums: List[int]) -> int:
         LIS = [1] * len(nums)
 
         for i in range(len(nums), -1, -1):
@@ -19,3 +20,13 @@ class Solution:
                 if nums[i] < nums[j]:
                     LIS[i] = max(LIS[i], 1 + LIS[j])
         return(max(LIS))
+
+    def lengthOfLIS3(self, nums: List[int]) -> int:
+        sub = []
+        for x in nums:
+            if len(sub) == 0 or sub[-1] < x:
+                sub.append(x)
+            else:
+                idx = bisect_left(sub, x)
+                sub[idx] = x
+        return len(sub)
