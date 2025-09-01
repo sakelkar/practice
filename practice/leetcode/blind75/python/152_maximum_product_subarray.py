f084e0832490cefacfdc0a778717e0820e2519da
Sameer Kelkar <sakelkar@cisco.com>
2025-09-01 21:29:20 +0530

    blind 75 leetcode 152,153 problems

--- practice/leetcode/blind75/python/152_maximum_product_subarray.py
+++ practice/leetcode/blind75/python/152_maximum_product_subarray.py
@@ -37,3 +37,21 @@ class Solution:
             suffix = suffix or 1 * nums[~1]
             max_so_far = max(max_so_far, prefix, suffix)
         return(max_so_far)
+
+    def maxProductSimple3(self, nums: List[int]) -> int:
+        prefix, suffix, max_so_far = 0, 0, float('-inf')
+        for i in range(len(nums)):
+            prefix = (prefix or 1) * nums[i]
+            suffix = (suffix or 1) * nums[~i]
+            max_so_far = max(max_so_far, prefix, suffix)
+        return(max_so_far)
+
+    def maxProduct3(self, nums: List[int]) -> int:
+        A = nums
+        B = nums[::-1]
+        for i in range(1, len(A)):
+            A[i] *= A[i-1] or 1
+            B[i] *= B[i-1] or 1
+
+        #at the end of for loop, A and B contain prefix product and suffix product
+        return(max(A+B))
